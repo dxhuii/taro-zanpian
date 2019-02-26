7b5e86c5cd1dfa2f149ab8b3e37f95c5629f978f
丁效辉 <dxhuii@qq.com>
2019-02-26 18:13:07 +0800

    设置alioss

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const config = {
   projectName: 'taro-99496',
   date: '2019-2-19',
@@ -31,6 +33,9 @@ const config = {
     ],
     options: {
     }
+  },
+  alias: {
+    '@': path.resolve(__dirname, '..', 'src')
   },
   weapp: {
     module: {
--- src/app.js
+++ src/app.js
@@ -2,17 +2,17 @@ import '@tarojs/async-await'
 import Taro, { Component } from '@tarojs/taro'
 import { Provider } from '@tarojs/redux'
 
-import Index from './pages/index'
+import Index from '@/pages/index'
 
-import configStore from './store'
+import configStore from '@/store'
 
 import './app.scss'
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
-// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
-//   require('nerv-devtools')
-// }
+if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+  require('nerv-devtools')
+}
 
 const store = configStore()
 
