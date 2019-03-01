57e8485df7d71a1336ccc1ab3168fb9cfcabb6b4
丁效辉 <dxhuii@qq.com>
2019-03-01 18:17:10 +0800

    删除无用文件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,29 +1,10 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
-
-import { add, minus, asyncAdd } from '@/store/actions/counter'
+import { View, Text } from '@tarojs/components'
 
 import Week from '@/components/Week'
 
 import './index.scss'
 
-@connect(
-  ({ counter }) => ({
-    counter
-  }),
-  dispatch => ({
-    add() {
-      dispatch(add())
-    },
-    dec() {
-      dispatch(minus())
-    },
-    asyncAdd() {
-      dispatch(asyncAdd())
-    }
-  })
-)
 class Index extends Component {
   config = {
     navigationBarTitleText: '首页'
@@ -48,26 +29,11 @@ class Index extends Component {
   render() {
     return (
       <View className='index'>
-        <Week type={1} />
-        <Week type={0} />
-        <Button className='add_btn' onClick={this.props.add}>
-          +
-        </Button>
-        <Button className='dec_btn' onClick={this.props.dec}>
-          -
-        </Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>
-          async
-        </Button>
-        <View>
-          <Text>{this.props.counter.num}</Text>
-        </View>
-        <View>
-          <Text>Hello, World</Text>
-        </View>
         <View onClick={this.handleClick.bind(this, '/pages/top/index')}>
           <Text>去top</Text>
         </View>
+        <Week type={1} />
+        <Week type={0} />
       </View>
     )
   }
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-import counter from './counter'
 import page from './page'
 import week from './week'
 
 export default combineReducers({
-  counter,
   page: page(),
   week: week()
 })
